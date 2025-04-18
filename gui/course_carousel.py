bd451fe1adff8d64dd512d418686c9f9326498b8
ddavidd <kristiandavidbautista@gmail.com>
2025-04-19 00:13:36 +0800

    added carousel for course selection with nav buttons + assets

---
+++ gui/course_carousel.py
@@ -0,0 +1,90 @@
+from PyQt5.QtWidgets import QWidget, QScrollArea, QHBoxLayout, QPushButton, QSizePolicy
+from PyQt5.QtCore import Qt, QPropertyAnimation, QEasingCurve, QAbstractAnimation
+from PyQt5.QtGui import QFont
+from constants import *
+
+class CourseCarousel(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.layout = QHBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        self.scroll = QScrollArea()
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setAlignment(Qt.AlignCenter)
+
+        container = QWidget()
+        container.setFixedHeight(63)
+
+        self.courses_layout = QHBoxLayout(container)
+        self.courses_layout.setContentsMargins(0, 0, 0, 0)
+        self.courses_layout.setSpacing(0)
+        self.courses_layout.setAlignment(Qt.AlignCenter)
+
+        course_names = [
+            "TECHNO CCS103", "APPDEV CS43", "PYTHON CS48",
+            "PDCOM CS51", "AUTOMATA CS23", "ML CS98", "DBSYS CS33"
+        ]
+
+        for course in course_names:
+            btn = self.create_course_box(course)
+            self.courses_layout.addWidget(btn)
+
+        self.scroll.setWidget(container)
+        self.layout.addWidget(self.scroll)
+        
+        # Setup animation
+        self.scroll_animation = QPropertyAnimation(self.scroll.horizontalScrollBar(), b"value")
+        self.scroll_animation.setEasingCurve(QEasingCurve.OutCubic)
+        self.scroll_animation.setDuration(300) 
+
+    def create_course_box(self, name):
+        btn = QPushButton(name)
+        btn.setFixedSize(320, 63)
+        btn.setFont(QFont(HEADING_FONT_MEDIUM, 16))
+        btn.setStyleSheet(f"""
+            QPushButton {{
+                background-color: white;
+                color: black;
+                border: 0px solid #ccc;
+                padding: 0px;
+            }}
+            QPushButton:hover {{
+                background-color: {MID_TEAL};
+                color: {SNOW_WHITE};
+            }}
+        """)
+
+        btn.setProperty("alignment", Qt.AlignCenter)
+        btn.setProperty("textAlignment", Qt.AlignCenter)
+        btn.setContentsMargins(0, 0, 0, 0)
+
+        return btn
+
+    def scroll_right(self):
+        current_value = self.scroll.horizontalScrollBar().value()
+        
+        max_value = self.scroll.horizontalScrollBar().maximum()
+        target_value = min(current_value + 330, max_value)
+        
+        if self.scroll_animation.state() == QAbstractAnimation.Running:
+            self.scroll_animation.stop()
+            
+        self.scroll_animation.setStartValue(current_value)
+        self.scroll_animation.setEndValue(target_value)
+        self.scroll_animation.start()
+    
+    def scroll_left(self):
+        current_value = self.scroll.horizontalScrollBar().value()
+        
+        target_value = max(current_value - 330, 0)
+        
+        if self.scroll_animation.state() == QAbstractAnimation.Running:
+            self.scroll_animation.stop()
+            
+        self.scroll_animation.setStartValue(current_value)
+        self.scroll_animation.setEndValue(target_value)
+        self.scroll_animation.start()
--- gui/footer_widget.py
+++ gui/footer_widget.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QSizePolicy, QLabel
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QCursor
 from constants import *
 from ui_components import create_section, create_separator
+from course_carousel import CourseCarousel
 
 class FooterWidget(QWidget):
     def __init__(self, parent=None):
@@ -24,19 +26,62 @@ class FooterWidget(QWidget):
         home_section.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
         layout.addWidget(home_section)
         
-        # Separator
-        layout.addWidget(create_separator("vertical", "white"))
-        
         # Course Section
-        course_section, _ = create_section("rgba(255, 255, 255, 0.2)", "COURSE_SECTION")
+        course_section, course_layout = create_section("rgba(255, 255, 255, 0.2)")
         course_section.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+    
+        # Configure the course layout for maximum carousel space
+        course_layout.setContentsMargins(0, 0, 0, 0)
+        course_layout.setSpacing(0)
+        course_layout.setAlignment(Qt.AlignCenter) 
+    
+        # Create carousel and make it take up the entire space
+        self.carousel = CourseCarousel()
+        self.carousel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.carousel.setMinimumSize(1760, 63)  # Set the desired size
+        course_layout.addWidget(self.carousel)
+
         layout.addWidget(course_section)
         
         # Separator
         layout.addWidget(create_separator("vertical", "white"))
         
         # Next Section
-        next_section, _ = create_section("rgba(255, 255, 255, 0.1)", "NEXT")
+        next_section, next_layout = create_section("rgba(255, 255, 255, 0.1)")
         next_section.setFixedWidth(NEXT_SECTION_WIDTH)
         next_section.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+
+        # Create a vertical layout to hold both icons
+        nav_layout = QHBoxLayout()
+        nav_layout.setAlignment(Qt.AlignCenter)
+        nav_layout.setSpacing(10)  # space between buttons
+
+        # Scroll Left Icon
+        left_icon = QLabel()
+        left_icon.setPixmap(QPixmap(r"assets\icons\navigate_before.svg").scaled(20, 20, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        left_icon.setCursor(QCursor(Qt.PointingHandCursor))
+        left_icon.mousePressEvent = self.scroll_left
+
+        # Scroll Right Icon
+        right_icon = QLabel()
+        right_icon.setPixmap(QPixmap(r"assets\icons\navigate_next.svg").scaled(20, 20, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        right_icon.setCursor(QCursor(Qt.PointingHandCursor))
+        right_icon.mousePressEvent = self.scroll_right
+
+        # Add icons to the nav layout
+        nav_layout.addWidget(left_icon)
+        nav_layout.addWidget(right_icon)
+
+        # Add nav layout to the next section
+        next_layout.addLayout(nav_layout)
+
+        # Add the final section to the main layout
         layout.addWidget(next_section)
+
+    def scroll_right(self, _):
+        if hasattr(self, 'carousel'):
+            self.carousel.scroll_right()
+
+    def scroll_left(self, _):
+        if hasattr(self, 'carousel'):
+            self.carousel.scroll_left()
