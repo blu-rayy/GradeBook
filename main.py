d5f72d286320c72e25cecf658cf0f3ecdb3c21ea
ddavidd <kristiandavidbautista@gmail.com>
2025-04-28 22:20:55 +0800

    added requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from PyQt5.QtWidgets import QApplication
+
 from load_utils import *
 from gui.main_window import MainWindow
 from gui.config_loader import UI_CONFIG
@@ -9,6 +10,8 @@ os.environ["QT_LOGGING_RULES"] = "qt.qpa.fonts=false"
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    ## pip install -r requirements.txt
 
     load_fonts()
     load_tables(r'db/gradebook.db', r'db/schema.sql')
